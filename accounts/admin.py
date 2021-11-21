a2826978711b0d123cd5051ce694c0320a96188b
MrMohammadY <m.yazdani000@gmail.com>
2021-11-21 22:37:48 +0330

    Update(accounts/models): create accounts models

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
+from django.contrib.auth import get_user_model
 
-# Register your models here.
+from accounts.models import JobSeeker, Employer
+
+User = get_user_model()
+
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('id', 'username', 'email', 'first_name', 'last_name',
+                    'date_joined', 'is_staff', 'is_active', 'user_type')
+    list_display_links = ('id', 'username', 'email')
+    list_filter = ('user_type',)
+    search_fields = ('username', 'email')
+
+
+@admin.register(JobSeeker)
+class JobSeekerAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Employer)
+class EmployerAdmin(admin.ModelAdmin):
+    pass
--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,42 @@
+from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.validators import ASCIIUsernameValidator
+from django.contrib.postgres.fields import CICharField
+
+from django.utils.translation import ugettext_lazy as _
 from django.db import models
 
-# Create your models here.
+from lib.models import BaseModel
+
+
+class CustomUser(AbstractUser):
+    username_validator = ASCIIUsernameValidator()
+
+    username = CICharField(
+        _('username'),
+        max_length=150,
+        unique=True,
+        help_text=_('Required. 150 characters or fewer. Letters, digits and @/./+/-/_ only.'),
+        validators=[username_validator],
+        error_messages={
+            'unique': _("A user with that username already exists."),
+        },
+    )
+    email = models.EmailField(_('email address'), unique=True)
+    phone_number = models.CharField(max_length=9, verbose_name=_('phone number'), blank=True)
+    is_employer = models.BooleanField(default=False, verbose_name=_('is employer'))
+    is_job_seeker = models.BooleanField(default=False, verbose_name=_('is job seeker'))
+
+    class Meta:
+        verbose_name = _('user')
+        verbose_name_plural = _('users')
+        db_table = 'user'
+
+
+class Employer(BaseModel):
+    user = models.OneToOneField(CustomUser, on_delete=models.PROTECT, primary_key=True, verbose_name=_('user'))
+    # TODO-1 : add company field here
+
+
+class JobSeeker(BaseModel):
+    user = models.OneToOneField(CustomUser, on_delete=models.PROTECT, primary_key=True, verbose_name=_('user'))
+    birthday = models.DateField(verbose_name=_('birthday'), blank=True, null=True)
