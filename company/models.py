c4c89306ef444d30a4d2fe6c05ebcfdbe66d844f
MrMohammadY <m.yazdani000@gmail.com>
2021-11-21 22:54:58 +0330

    Update(company): update company models

--- company/models.py
+++ company/models.py
@@ -1,4 +1,4 @@
-import datetime
+import khayyam
 
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
@@ -10,8 +10,8 @@ class CompanyType(BaseModel):
     type = models.CharField(max_length=60, verbose_name=_('type'))
 
     class Meta:
-        verbose_name = _('CompanyType')
-        verbose_name_plural = _('CompanyTypes')
+        verbose_name = _('company type')
+        verbose_name_plural = _('company types')
         db_table = 'company_type'
 
 
@@ -19,29 +19,34 @@ class Company(BaseModel):
 
     @staticmethod
     def year_choices():
-        return [(r, r) for r in range(1984, datetime.date.today().year + 1)]
+        return tuple([(y, y) for y in range(1280, khayyam.JalaliDatetime.today().year + 1)])
 
     persian_name = models.CharField(max_length=150, verbose_name=_('persian name'))
     english_name = models.CharField(max_length=150, verbose_name=_('english name'))
-    foundation = models.IntegerField(_('year'), choices=year_choices)
-    site = models.URLField(verbose_name=_('site'), null=True, blank=True)
+    foundation = models.PositiveSmallIntegerField(_('year'), choices=year_choices)
+    site = models.URLField(verbose_name=_('site'), blank=True)
     logo = models.ImageField(upload_to='company/logos/', verbose_name=_('logo'))
     banner = models.ImageField(upload_to='company/banners/', verbose_name=_('banner'))
     type = models.ForeignKey(CompanyType, on_delete=models.PROTECT, related_name='companies', verbose_name='type')
     number_of_employees = models.PositiveSmallIntegerField(verbose_name=_('number of employees'))
     description = models.TextField(verbose_name=_('description'))
     # TODO-1 : create city field here
-    introduction = models.TextField(verbose_name=_('introduction'), blank=True, null=True)
-    culture = models.TextField(verbose_name=_('culture'), blank=True, null=True)
-    advantage = models.TextField(verbose_name=_('advantage'), blank=True, null=True)
+    introduction = models.TextField(verbose_name=_('introduction'), blank=True)
+    culture = models.TextField(verbose_name=_('culture'), blank=True)
+    advantage = models.TextField(verbose_name=_('advantage'), blank=True)
+
+    class Meta:
+        verbose_name = _('company')
+        verbose_name_plural = _('companies')
+        db_table = 'company'
 
 
 class EmployeeType(BaseModel):
     type = models.CharField(max_length=60, verbose_name=_('type'))
 
     class Meta:
-        verbose_name = _('Type')
-        verbose_name_plural = _('Types')
+        verbose_name = _('type')
+        verbose_name_plural = _('types')
         db_table = 'type'
 
 
@@ -58,6 +63,6 @@ class Employee(BaseModel):
     )
 
     class Meta:
-        verbose_name = _('Employee')
-        verbose_name_plural = _('Employees')
+        verbose_name = _('employee')
+        verbose_name_plural = _('employees')
         db_table = 'employee'
