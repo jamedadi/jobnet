f494ff7eba040021a86622eccf503e284afa6d96
MrMohammadY <m.yazdani000@gmail.com>
2021-11-21 22:38:16 +0330

    Update(settings): change defualt user auth to custom user

--- jobnet/settings.py
+++ jobnet/settings.py
@@ -29,6 +29,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'accounts.apps.AccountsConfig',
 ]
 
 MIDDLEWARE = [
@@ -115,3 +117,4 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+AUTH_USER_MODEL = 'accounts.CustomUser'
