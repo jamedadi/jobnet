3b4b33278af66b751a7a9cb8011effe2717ae75d
AmirHossein Bonakdar <amirhossein.bonakdar@yahoo.com>
2021-11-22 08:06:37 +0330

    add upload to for resume file of Resume model

--- resume/models.py
+++ resume/models.py
@@ -8,7 +8,7 @@ from django.utils.translation import ugettext_lazy as _
 class Resume(BaseModel):
     job_seeker = models.ForeignKey(to=JobSeeker, verbose_name=_('job seeker'), related_name='resumes',
                                    on_delete=models.CASCADE)
-    file = models.FileField(verbose_name=_('file'))
+    file = models.FileField(verbose_name=_('file'), upload_to='resumes/')
 
     def __str__(self):
         return self.file.url
