6fb70600743b6d04453cd0a0fc1240225e8cd716
AmirHossein Bonakdar <amirhossein.bonakdar@yahoo.com>
2021-11-23 07:31:05 +0330

    add slug field for State and City models

--- address/models.py
+++ address/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils.text import slugify
 
 from lib.models import BaseModel
 from django.utils.translation import ugettext_lazy as _
@@ -6,9 +7,15 @@ from django.utils.translation import ugettext_lazy as _
 
 class State(BaseModel):
     name = models.CharField(max_length=20, verbose_name=_('name'), unique=True)
+    slug = models.SlugField(verbose_name=_('slug'), unique=True, allow_unicode=True)
 
     def __str__(self):
         return self.name
+
+    def save(self, *args, **kwargs):
+        if not self.pk:
+            self.slug = slugify(self.name, allow_unicode=True)
+        super().save(*args, **kwargs)
 
     class Meta:
         verbose_name = _('state')
@@ -18,10 +25,16 @@ class State(BaseModel):
 
 class City(BaseModel):
     name = models.CharField(max_length=20, verbose_name=_('name'))
+    slug = models.SlugField(verbose_name=_('slug'), unique=True, allow_unicode=True)
     state = models.ForeignKey(to=State, verbose_name=_('state'), related_name='cities', on_delete=models.CASCADE)
 
     def __str__(self):
         return f'{self.name}'
+
+    def save(self, *args, **kwargs):
+        if not self.pk:
+            self.slug = slugify(self.name + self.state.slug, allow_unicode=True)
+        super().save(*args, **kwargs)
 
     class Meta:
         verbose_name = _('city')
