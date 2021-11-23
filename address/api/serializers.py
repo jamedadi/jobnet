1139cf7a230ae73be639ae7125958a5600c652b3
AmirHossein Bonakdar <amirhossein.bonakdar@yahoo.com>
2021-11-23 09:40:37 +0330

    add get_sate mehtod for CitySerializer

--- address/api/serializers.py
+++ address/api/serializers.py
@@ -6,10 +6,18 @@ from address.models import State, City
 class StateSerializer(serializers.ModelSerializer):
     class Meta:
         model = State
-        fields = ('name',)
+        fields = ('name', 'slug')
 
 
 class CitySerializer(serializers.ModelSerializer):
+    def get_state(self, obj):
+        return {
+            "name": obj.state.name,
+            "slug": obj.sate.slug
+        }
+
+    author = serializers.SerializerMethodField('get_state')
+
     class Meta:
         model = City
-        fields = ('state', 'name')
+        fields = ('name', 'slug')
