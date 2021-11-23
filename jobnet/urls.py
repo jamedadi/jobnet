630ea23108a93179a7d28ad63190c88eefc54124
AmirHossein Bonakdar <amirhossein.bonakdar@yahoo.com>
2021-11-23 11:40:50 +0330

    add urls of address app

--- jobnet/urls.py
+++ jobnet/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/address/', include('address.api.urls')),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
