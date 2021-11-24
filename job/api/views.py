c4f559a3e76805eeeb6c1742ea9d0cd20317de3f
AmirHossein Bonakdar <amirhossein.bonakdar@yahoo.com>
2021-11-24 08:15:24 +0330

    add viewsets for job app

---
+++ job/api/views.py
@@ -0,0 +1,24 @@
+from rest_framework.viewsets import ModelViewSet
+
+from job.api.permissions import IsEmployerOrReadOnly
+from job.api.serializers import JobCategorySerializer, SkillSerializer, ReadJobSerializer
+from job.models import JobCategory, Skill, Job
+from lib.api.permissions import IsAdminOrReadOnly
+
+
+class JobCategoryViewSet(ModelViewSet):
+    queryset = JobCategory.objects.all()
+    serializer_class = JobCategorySerializer
+    permission_classes = [IsAdminOrReadOnly]
+
+
+class SkillViewSet(ModelViewSet):
+    queryset = Skill.objects.all()
+    serializer_class = SkillSerializer
+    permission_classes = [IsAdminOrReadOnly]
+
+
+class JobViewSet(ModelViewSet):
+    queryset = Job.objects.all()
+    serializer_class = ReadJobSerializer
+    permission_classes = [IsEmployerOrReadOnly]
