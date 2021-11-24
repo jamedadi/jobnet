b6e58bff1585160c6efba165580c72df68897c4c
AmirHossein Bonakdar <amirhossein.bonakdar@yahoo.com>
2021-11-24 08:16:21 +0330

    add session authentication to list of authentication classes

--- jobnet/settings.py
+++ jobnet/settings.py
@@ -103,6 +103,7 @@ AUTH_USER_MODEL = 'accounts.CustomUser'
 # django-rest-framework
 
 REST_FRAMEWORK = {
-    'DEFAULT_AUTHENTICATION_CLASSES': ['rest_framework_simplejwt.authentication.JWTAuthentication'],
+    'DEFAULT_AUTHENTICATION_CLASSES': ['rest_framework_simplejwt.authentication.JWTAuthentication',
+                                       'rest_framework.authentication.SessionAuthentication'],
     'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
 }
