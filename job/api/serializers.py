d6bc1c4cd840c67d29f78c56f74faa94f484d337
AmirHossein Bonakdar <amirhossein.bonakdar@yahoo.com>
2021-11-24 09:58:30 +0330

    add id for SkillSerializer , JobCategorySerializer

--- job/api/serializers.py
+++ job/api/serializers.py
@@ -6,17 +6,16 @@ from job.models import JobCategory, Skill, Job
 class JobCategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = JobCategory
-        fields = ('name',)
+        fields = ('id', 'name')
 
 
 class SkillSerializer(serializers.ModelSerializer):
     class Meta:
         model = Skill
-        fields = ('title',)
+        fields = ('id', 'title')
 
 
 class ReadJobSerializer(serializers.ModelSerializer):
-    
     class Meta:
         model = Job
         fields = '__all__'
