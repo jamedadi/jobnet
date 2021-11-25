56713c30e6798bba9f4eb6a551fd2ee1d0c22d4b
MrMohammadY <m.yazdani000@gmail.com>
2021-11-25 14:28:30 +0330

    Update(urls): add company urls in base

--- jobnet/urls.py
+++ jobnet/urls.py
@@ -4,6 +4,7 @@ from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/company/', include('company.api.urls')),
     path('api/address/', include('address.api.urls')),
     path('api/job/', include('job.api.urls')),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
