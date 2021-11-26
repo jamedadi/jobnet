43a2ffed4ac4485c594dda0acf910431dc87c404
MrMohammadY <m.yazdani000@gmail.com>
2021-11-26 13:48:06 +0330

    Create(accounts/api/urls): create jobseeker and employer view url

--- accounts/api/urls.py
+++ accounts/api/urls.py
@@ -3,15 +3,16 @@ from django.urls import path, include
 from rest_framework import routers
 
 from accounts.api.views import UserRegistrationCreateApiView, UserChangePasswordUpdateApiView, \
-    UserUpdateDetailUpdateApiView
+    JobSeekerAPIView, EmployerAPIView
 
 app_name = 'accounts'
 
 router = routers.SimpleRouter()
-router.register('', UserUpdateDetailUpdateApiView)
+router.register('job-seeker', JobSeekerAPIView)
+router.register('employer', EmployerAPIView)
 
 urlpatterns = [
     path('registration/<str:user_type>/', UserRegistrationCreateApiView.as_view(), name='registration'),
     path('change-password/', UserChangePasswordUpdateApiView.as_view(), name='change-password'),
-    path('update/', include(router.urls), name='update'),
+    path('', include(router.urls)),
 ]
