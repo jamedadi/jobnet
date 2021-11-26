f797cdeebb83edae524393b9eb161dee81f0a7de
MrMohammadY <m.yazdani000@gmail.com>
2021-11-26 13:47:17 +0330

    Create(accounts/api/permissions): create is jobseeker and is employer permissions

--- accounts/api/permissions.py
+++ accounts/api/permissions.py
@@ -13,3 +13,25 @@ class UserObjectOwner(BasePermission):
         if request.method in SAFE_METHODS:
             return True
         return bool(request.user.id == obj.id)
+
+
+class IsJobSeeker(BasePermission):
+    def has_permission(self, request, view):
+        if request.method in SAFE_METHODS:
+            return True
+        return bool(request.user.is_authenticated and request.user.is_job_seeker)
+
+    def has_object_permission(self, request, view, obj):
+        if request.method in SAFE_METHODS:
+            return True
+        return bool(request.user.is_authenticated and request.user.is_job_seeker and request.user == obj.user)
+
+
+class IsEmployer(BasePermission):
+    def has_permission(self, request, view):
+        return bool(request.user.is_authenticated and request.user.is_employer)
+
+    def has_object_permission(self, request, view, obj):
+        if request.method in SAFE_METHODS:
+            return True
+        return bool(request.user.is_authenticated and request.user.is_employer and request.user == obj.user)
