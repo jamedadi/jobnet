93669f9c244d1847e1371c8c34136cc861d6e664
MrMohammadY <m.yazdani000@gmail.com>
2021-11-27 19:53:29 +0330

    Create(accounts/api/urls): create employee url

--- company/api/urls.py
+++ company/api/urls.py
@@ -1,12 +1,14 @@
 from django.urls import path, include
 from rest_framework import routers
 
-from company.api.views import CompanyModelViewSetAPI, CompanyTypeModelViewSetAPI, EmployeeTypeModelViewSetAPI
+from company.api.views import CompanyModelViewSetAPI, CompanyTypeModelViewSetAPI, EmployeeTypeModelViewSetAPI, \
+    EmployeeModelViewSetAPI
 
 app_name = 'company'
 
 router = routers.SimpleRouter()
 
+router.register('employee', EmployeeModelViewSetAPI)
 router.register('', CompanyModelViewSetAPI)
 router.register('type/company', CompanyTypeModelViewSetAPI)
 router.register('type/employee', EmployeeTypeModelViewSetAPI)
