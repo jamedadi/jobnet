71452bcbb94ce2893e4231fed11de771b5a1bd5e
MrMohammadY <m.yazdani000@gmail.com>
2021-11-27 19:26:30 +0330

    Create(accounts/api/views): create company type view

--- company/api/views.py
+++ company/api/views.py
@@ -1,10 +1,11 @@
-from rest_framework.viewsets import ModelViewSet
+from rest_framework.viewsets import ModelViewSet, GenericViewSet
+from rest_framework import mixins
 
 from company.api.filters import CompanyFilter
-from company.api.serializers import CompanySerializer
-from company.models import Company
+from company.api.serializers import CompanySerializer, CompanyTypeSerializer
+from company.models import Company, CompanyType
 
-from lib.api.permissions import IsObjectEmployerOrReadOnly
+from lib.api.permissions import IsObjectEmployerOrReadOnly, IsEmployer
 
 
 class CompanyModelViewSetAPI(ModelViewSet):
@@ -16,3 +17,11 @@ class CompanyModelViewSetAPI(ModelViewSet):
 
     def perform_create(self, serializer):
         serializer.save(employer=self.request.user.employer)
+
+
+class CompanyTypeModelViewSetAPI(mixins.CreateModelMixin, mixins.RetrieveModelMixin,
+                                 mixins.ListModelMixin, GenericViewSet):
+    queryset = CompanyType.objects.all()
+    serializer_class = CompanyTypeSerializer
+    permission_classes = (IsEmployer,)
+    search_fields = ('type', )
