93eef1376368f50abb13766095fb6787dbac4e4c
AmirHossein Bonakdar <amirhossein.bonakdar@yahoo.com>
2021-11-27 22:33:58 +0330

    remove test task

--- jobnet/celery.py
+++ jobnet/celery.py
@@ -7,8 +7,3 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'jobnet.settings')
 app = Celery('jobnet')
 app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks()
-
-
-@app.task(bind=True)
-def debug_task(self):
-    print(f'Request: {self.request!r}')
