058c68bb72e55afbd9bc82d6b5b44dba1ed5e729
MrMohammadY <m.yazdani000@gmail.com>
2021-11-27 19:26:39 +0330

    Create(accounts/api/urls): create company type urls

--- company/api/urls.py
+++ company/api/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path, include
 from rest_framework import routers
 
-from company.api.views import CompanyModelViewSetAPI
+from company.api.views import CompanyModelViewSetAPI, CompanyTypeModelViewSetAPI
 
 app_name = 'company'
 
 router = routers.SimpleRouter()
+router.register('type', CompanyTypeModelViewSetAPI)
 router.register('', CompanyModelViewSetAPI)
 
 urlpatterns = [
