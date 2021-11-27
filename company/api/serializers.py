dc6d9d800df6936c314a18e09939bc228a1c6456
MrMohammadY <m.yazdani000@gmail.com>
2021-11-27 19:25:36 +0330

    Create(accounts/api/serializers): create company type serializer

--- company/api/serializers.py
+++ company/api/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-from company.models import Company
+from company.models import Company, CompanyType
 
 
 class CompanySerializer(serializers.ModelSerializer):
@@ -9,3 +9,10 @@ class CompanySerializer(serializers.ModelSerializer):
     class Meta:
         model = Company
         exclude = ('employer', 'modified_time')
+
+
+class CompanyTypeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CompanyType
+        fields = ('id', 'type',)
+        read_only_fields = ('id',)
