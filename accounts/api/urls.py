b36f796c890f2ec508c158609c2536ccb8931830
MrMohammadY <m.yazdani000@gmail.com>
2021-12-03 09:56:36 +0330

    Create(accounts/api/urls): create reset password urls

--- accounts/api/urls.py
+++ accounts/api/urls.py
@@ -3,7 +3,8 @@ from django.urls import path, include
 from rest_framework import routers
 
 from accounts.api.views import UserRegistrationCreateApiView, UserChangePasswordUpdateApiView, \
-    JobSeekerAPIView, EmployerAPIView, UserInfo, VerifyEmail, ResendEmail
+    JobSeekerAPIView, EmployerAPIView, UserInfo, VerifyEmail, ResendEmail, RequestResetEmailPasswordAPIView, \
+    ResetEmailPasswordAPIView
 
 app_name = 'accounts'
 
@@ -18,5 +19,7 @@ urlpatterns = [
     path('change-password/', UserChangePasswordUpdateApiView.as_view(), name='change-password'),
     path('verify-email/', VerifyEmail.as_view(), name='verify-email'),
     path('resend-email/', ResendEmail.as_view(), name='resend-email'),
-
+    path('verify-email/<uib64>/<token>/', VerifyEmail.as_view(), name='verify-email'),
+    path('request-reset-password/', RequestResetEmailPasswordAPIView.as_view(), name='request-reset-password'),
+    path('confirm-reset-password/<uib64>/<token>/', ResetEmailPasswordAPIView.as_view(), name='confirm-reset-password'),
 ]
