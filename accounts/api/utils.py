0864d105c9c9b2546fdedaa1d0385b71ba9218cd
MrMohammadY <m.yazdani000@gmail.com>
2021-12-03 09:59:48 +0330

    Feat(accounts/api/utils): update send email option

--- accounts/api/utils.py
+++ accounts/api/utils.py
@@ -1,14 +1,39 @@
 from django.contrib.auth.tokens import default_token_generator
-from django.conf import settings
+from django.urls import reverse
+from django.utils.encoding import smart_bytes
+from django.utils.http import urlsafe_base64_encode
 
 
-def create_activation_link(user):
-    confirmation_token = default_token_generator.make_token(user)
-    return f'{settings.ACTIVATION_LINK_URL}?user_id={user.id}&confirmation_token={confirmation_token}'
+def generate_token_and_user_id_base64(user):
+    token = default_token_generator.make_token(user)
+    user_id_base64 = urlsafe_base64_encode(smart_bytes(user.id))
+    return {'uib64': user_id_base64, 'token': token}
 
 
-def send_verification_email(user):
-    user.email_user(
-        subject='Jobnet : verify your email',
-        message=create_activation_link(user)
-    )
+def reset_password_link_generator(user):
+    relative_link = reverse('accounts:confirm-reset-password', kwargs=generate_token_and_user_id_base64(user))
+    link = 'http://127.0.0.1:8000' + relative_link
+    return f'Hi {user.username} this link for reset your password: {link}'
+
+
+def email_verification_link_generator(user):
+    relative_link = reverse('accounts:verify-email', kwargs=generate_token_and_user_id_base64(user))
+    link = 'http://127.0.0.1:8000' + relative_link
+    return f'Hi {user.username} this link for verify your email: {link}'
+
+
+def body_generator(user, email_type):
+    if email_type == 'email_verification':
+        return {
+            'subject': 'Job net(جاب نت): verify your email',
+            'message': email_verification_link_generator(user)
+        }
+    elif email_type == 'reset_password':
+        return {
+            'subject': 'Job net(جاب نت): reset password',
+            'message': reset_password_link_generator(user)
+        }
+
+
+def send_email(user, email_type):
+    user.email_user(**body_generator(user, email_type))
